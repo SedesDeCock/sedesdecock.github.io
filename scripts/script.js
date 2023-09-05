a553a6ac843352b1cb8c17c414a60e15b48aed93
ArneSamson <arnesamson007@gmail.com>
2023-09-05 14:41:22 +0200

    working links in navbar to linkedIn and itchiIO

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+const itchio = document.getElementById('itchio-button');
+const linkedin = document.getElementById('linkedin-button');
+
+itchio.addEventListener('click', () => {
+    window.open('https://sedesdecock.itch.io');
+});
+linkedin.addEventListener('click', () => {
+    window.open('https://www.linkedin.com/in/sedes-de-cock-90070228a/');
+});
